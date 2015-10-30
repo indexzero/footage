fe81d3f1ebebe204178039d6bedf30a1941a3e01
indexzero <charlie.robbins@gmail.com>
2015-10-29 23:16:47 -0700

    [api bin] Initial implementation.

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var async = require('async'),
+    concat = require('concat-stream'),
+    hyperquest = require('hyperquest'),
+    groupsOf = require('in-groups-of');
+
+var debug = require('diagnostics')('footage');
+
+var footage = module.exports = function (user) {
+  var searchUri = 'https://skimdb.npmjs.com/registry/_design/app/_view/byUser?key='
+
+  hyperquest.get(searchUri + '%22' + user + '%22')
+    .pipe(concat({ encoding: 'string' }, function (data) {
+      footage.downloads(JSON.parse(data));
+    }));
+};
+
+footage.downloads = function (pkgs) {
+  var baseUri = 'https://api.npmjs.org/downloads/range';
+  var groups = groupsOf(pkgs.rows.map(function (row) {
+    return row.id;
+  }).sort(), 50);
+
+  //
+  // Does a simple reduce on the download
+  // keys of a single npm downloads request
+  //
+  function sumDownloads(names, stats) {
+    names.forEach(function (name) {
+      if (!stats[name]) { return; }
+      stats[name].total = Object.keys(stats[name].downloads || {})
+        .reduce(function (sum, key) {
+          return sum + stats[name].downloads[key].downloads;
+        }, 0);
+    });
+
+    return stats;
+  }
+
+  //
+  // Gets the statistics for the specified
+  // set of packages.
+  //
+  function getGroupStats(query) {
+    return function (pkgs, next) {
+      var uri = [baseUri, query, pkgs.join(',')].join('/');
+      debug('GET ' + uri);
+      hyperquest.get(uri)
+        .pipe(concat({ encoding: 'string' }, function (data) {
+          next(null, sumDownloads(pkgs, JSON.parse(data)));
+        }));
+    };
+  }
+
+  async.mapLimit(groups, 5, getGroupStats('last-month'), function (err, mapped) {
+    var all = mapped.reduce(function (acc, group) {
+      Object.keys(group).forEach(function (name) {
+        acc[name] = group[name];
+      });
+
+      return acc;
+    }, {});
+
+    Object.keys(all)
+      .sort(function (lname, rname) {
+        return all[lname].total - all[rname].total;
+      })
+      .forEach(function (name) {
+        console.log(name, all[name].total);
+      });
+  });
+};
