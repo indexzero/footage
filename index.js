3663c31a47983d97c37538512c80a1b742c19b4a
indexzero <charlie.robbins@gmail.com>
2022-02-16 23:29:45 -0500

    refactor fix: works for scoped now

--- index.js
+++ index.js
@@ -1,88 +1,73 @@
-import mapLimit from 'async/mapLimit.js';
-import groupsOf from 'in-groups-of';
 import got from 'got';
 import Bourne from '@hapi/bourne';
+import mapLimit from 'async/mapLimit.js';
+import groupsOf from 'in-groups-of';
 
 import diagnostics from 'diagnostics';
 const debug = diagnostics('footage');
 
-//
 // Range cannot be larger than 365 days.
-//
 const RANGE = process.env.NPM_RANGE || 'last-month';
 
-export default async function footage(user) {
-  const pkgs = getSearchIterator({ user });
+// Naive regexp for separating scoped package names
+const isScoped = /^\@([^\/]+)\/([^\/]+)$/
 
-  for await (const pkg of pkgs) {
-    console.dir(pkg);
-  }
+// Static npm mirror URLs
+const URLS = {
+  SEARCH: 'https://registry.npmjs.org/-/v1/search',
+  DOWNLOADS: 'https://api.npmjs.org/downloads/point'
 };
 
 /**
- * Returns a Got-based iterator for returning pages of npm search results.
+ * { function_description }
  *
- * See also:
- *
- * https://nodejs.org/api/stream.html#writablewritechunk-encoding-callback
- * https://2ality.com/2019/11/nodejs-streams-async-iteration.html#writing-to-writable-streams
- * https://github.com/sindresorhus/got/blob/main/documentation/4-pagination.md
- * https://github.com/sindresorhus/got/blob/main/source/core/options.ts#L696
- *
- * @return {Iterator} Got-based iterator for abstracting over pages
+ * @param      {<type>}   user    The user
+ * @return     {Promise}  { description_of_the_return_value }
  */
-function getSearchIterator({ user, size = 100}) {
-  let from = 0;
+export default async function footage(user) {
+  const results = getSearchIterator({ user });
+  const pkgs = {
+    scoped: [],
+    unscoped: []
+  };
 
-  return got.paginate('https://registry.npmjs.org/-/v1/search', {
-    method: 'get',
-    searchParams: {
-      text: `maintainer:${user}`,
-      from,
-      size
-    },
-    pagination: {
-      paginate: ({ response, currentItems }) => {
-        // If there are no more data, finish.
-        if (currentItems.length === 0) {
-          return false;
-        }
+  for await (const { package: pkg } of results) {
+    const cohort = isScoped.test(pkg.name) ? 'scoped' : 'unscoped';
+    pkgs[cohort].push(pkg);
+  }
 
-        // Increment the current page number.
-        from += size;
-        console.log({ from, size });
+  await downloadsFor({ pkgs, user });
+};
 
-        // Update the cursor by size in searchParams
-        return {
-          searchParams: {
-            text: `maintainer:${user}`,
-            from,
-            size
-          }
-        };
-      },
-      // Using `Bourne` to prevent prototype pollution.
-      transform: response => {
-        const res = Bourne.parse(response.body);
-        return res.objects;
-      },
-      // Wait 1s before making another request to prevent API rate limiting.
-      backoff: 1000,
-      // It is a good practice to set an upper limit of how many requests can be made.
-      // This way we can avoid infinite loops.
-      requestLimit: 285,
-      // In this case, we don't need to store all the items we receive.
-      // They are processed immediately.
-      stackAllItems: false
-    }
+
+async function downloadsFor({ pkgs, user }) {
+  const { scoped, unscoped } = pkgs;
+  await Promise.all([
+    downloadsForScoped({ pkgs: scoped, user }),
+    //downloadsForUnscoped({ pkgs: unscoped, user })
+  ]);
+
+};
+
+async function downloadsForScoped({ pkgs, user }) {
+  const names = pkgs.map(p => p.name).sort();
+
+  debug('Downloads for:', names);
+
+  const dls = await mapLimit(names, 10, async function dlsFor(name) {
+    const res = await got(`${URLS.DOWNLOADS}/${RANGE}/${name}`).json();
+    console.log(res);
+    return res;
   });
 }
 
-footage.downloads = async function downloads(pkgs, user) {
-  console.dir(pkgs.length);
+async function downloadsForUnscoped({ pkgs, user }) {
   const names = pkgs.map(p => p.name).sort();
-  console.dir(names);
+
+  debug('Downloads for:', names);
   const groups = groupsOf(names, 50);
+
+
 
   // //
   // // Does a simple reduce on the download
@@ -138,3 +123,56 @@ footage.downloads = async function downloads(pkgs, user) {
   //   console.log(user, userTotal);
   // });
 };
+
+/**
+ * Returns a Got-based iterator for returning pages of npm search results.
+ *
+ * See also:
+ *
+ * https://nodejs.org/api/stream.html#writablewritechunk-encoding-callback
+ * https://2ality.com/2019/11/nodejs-streams-async-iteration.html#writing-to-writable-streams
+ * https://github.com/sindresorhus/got/blob/main/documentation/4-pagination.md
+ * https://github.com/sindresorhus/got/blob/main/source/core/options.ts#L696
+ *
+ * @return {Iterator} Got-based iterator for abstracting over pages
+ */
+function getSearchIterator({ user, size = 100 }) {
+  const searchParams = {
+    text: `maintainer:${user}`,
+    from: 0,
+    size
+  };
+
+  debug(URLS.SEARCH, searchParams);
+  return got.paginate(URLS.SEARCH, {
+    method: 'get',
+    searchParams,
+    pagination: {
+      paginate: ({ response, currentItems }) => {
+        // If there are no more data, finish.
+        if (currentItems.length === 0) {
+          return false;
+        }
+
+        // Update the cursor by size in searchParams
+        searchParams.from += size;
+        debug(URLS.SEARCH, searchParams);
+
+        return { searchParams };
+      },
+      // Using `Bourne` to prevent prototype pollution.
+      transform: response => {
+        const res = Bourne.parse(response.body);
+        return res.objects;
+      },
+      // Wait 1s before making another request to prevent API rate limiting.
+      backoff: 1000,
+      // It is a good practice to set an upper limit of how many requests can be made.
+      // This way we can avoid infinite loops.
+      requestLimit: 285,
+      // In this case, we don't need to store all the items we receive.
+      // They are processed immediately.
+      stackAllItems: false
+    }
+  });
+}
